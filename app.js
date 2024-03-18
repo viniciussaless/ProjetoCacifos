3f3d5a0a0f7d16f5eac41ccb64cc32ef4c6ada85
viniciussaless <eeepvinicius273@gmail.com>
2024-03-18 11:54:32 +0000

    update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const { render } = require('ejs');
+var idUtilizador;
 
 app.get('/', (req, res) => {
   res.redirect('/login');
@@ -30,9 +31,12 @@ app.post('/login', (req, res) => { //rota para pegar o email e password do login
       res.status(500).send("Erro na consulta");
     } else {
       if(results.length > 0) {
-          const dbEmail = results[0].email_utilizador;
-          const dbPassword = results[0].password_utilizadores;
-          res.redirect("/index"); // Corrigido o redirecionamento
+        const dbidUtilizador = results[0].IdUtilizador;
+        idUtilizador = dbidUtilizador;
+        console.log(idUtilizador);
+        const dbEmail = results[0].email_utilizador;
+        const dbPassword = results[0].password_utilizadores;
+        res.redirect("/index"); // Corrigido o redirecionamento
       } else {
          
         res.status(401).send("Email ou senha incorretos");
