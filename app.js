f587baee63dfc470fdece6a3017e41ca0e5ebdb2
viniciussaless <eeepvinicius273@gmail.com>
2024-03-18 09:31:28 +0000

    update

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('Olá, mundo!');
 });
+
+app.get('/login', (req, res) => {
+    res.send('Olá, mundo!');
+});
+
+
+app.use(express.static('public')); //definição da pasta com arquivos estaticos
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
